0f3fefc32c03cc43f5fe7a503e826e47cf50b1da
Alexey Ozerov <alexozerov@gmail.com>
2019-05-28 23:47:03 +0300

    New Single View App created

---
+++ L001P001/L001P001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L001P001
+//
+//  Created by Alexey Ozerov on 28/05/2019.
+//  Copyright © 2019 Alexey Ozerov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
