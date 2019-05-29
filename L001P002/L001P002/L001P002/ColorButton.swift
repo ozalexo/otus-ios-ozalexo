66212b56d7093188252f3db9670b872943758810
Alexey Ozerov <alexozerov@gmail.com>
2019-05-29 16:02:05 +0300

    Added "ColorButton" class for the Button

---
+++ L001P002/L001P002/L001P002/ColorButton.swift
@@ -0,0 +1,21 @@
+//
+//  ColorButton.swift
+//  L001P002
+//
+//  Created by Alexey Ozerov on 29/05/2019.
+//  Copyright © 2019 Alexey Ozerov. All rights reserved.
+//
+
+import UIKit
+
+class ColorButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
