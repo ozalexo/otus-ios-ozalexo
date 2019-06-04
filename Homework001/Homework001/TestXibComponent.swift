616176b2b6a3b0c2057265638b6f56c2174010ae
Alexey Ozerov <alexozerov@gmail.com>
2019-06-04 05:01:44 +0300

    Used newly added XIB component to details storybook (split view).

---
+++ Homework001/Homework001/TestXibComponent.swift
@@ -0,0 +1,41 @@
+//
+//  TestXibComponent.swift
+//  Homework001
+//
+//  Created by Alexey Ozerov on 04/06/2019.
+//  Copyright © 2019 Alexey Ozerov. All rights reserved.
+//
+
+import UIKit
+
+class TestXibComponent: UIView {
+
+    private weak var view: UIView?
+    
+    @IBInspectable var bgColor: UIColor = .white {
+        didSet {
+            view?.backgroundColor = bgColor
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configure()
+    }
+    
+    private func configure () {
+        let bundle = Bundle.main
+        let nib = bundle.loadNibNamed("TestXibComponent", owner: nil, options: nil)
+        if let view = nib?[0] as? UIView
+        {
+            self.view = view
+            addSubview(view)
+        }
+    }
+
+}
