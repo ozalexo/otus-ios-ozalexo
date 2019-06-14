f140c9a5dea39df032b2f569debe8b2de026fe38
Alexey Ozerov <alexozerov@gmail.com>
2019-06-14 03:03:26 +0300

    Added methods viewWillDisappear and viewDidDisappear

--- L002P002/L002P002/ViewController.swift
+++ L002P002/L002P002/ViewController.swift
@@ -22,11 +22,23 @@ class ViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        print(#function)
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        print(#function)
         // Here we may start an animation
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#function)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#function)
     }
 
     @IBAction func ExitApp(_ sender: UIButton) {
