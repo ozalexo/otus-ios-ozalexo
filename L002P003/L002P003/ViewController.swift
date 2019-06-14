3003aba7ff4f53453af08764bed570451532ddb2
Alexey Ozerov <alexozerov@gmail.com>
2019-06-14 15:15:15 +0300

    Removed debug traces and LC methods

--- L002P003/L002P003/ViewController.swift
+++ L002P003/L002P003/ViewController.swift
@@ -10,57 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func loadView() {
-        super.loadView()
-        print(#function)
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#function)
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print(#function)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print(#function)
-        // Here we may start an animation
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print(#function)
-    }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print(#function)
-    }
-
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        print(#function)
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        print(#function)
-    }
-
-    @IBAction func ExitApp(_ sender: UIButton) {
-        print(#function)
-        // Calling 'exit' is very bad way to exit (aka crash)
-        // exit(-1)
-       
-        // Other ways also not welcome by Apple. Use 'Home' button or appropriate swipe gesture instead.
-        UIControl().sendAction(
-            #selector(URLSessionTask.suspend),
-            to: UIApplication.shared,
-            for: nil)
-    }
 }
 
