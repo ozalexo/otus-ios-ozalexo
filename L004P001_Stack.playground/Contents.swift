629720a0e4d0dbfb7ae8662a5b860504154d6fb4
Alexey Ozerov <alexozerov@gmail.com>
2019-07-17 15:02:35 +0300

    Added text label

--- L004P001_Stack.playground/Contents.swift
+++ L004P001_Stack.playground/Contents.swift
@@ -6,7 +6,7 @@ import PlaygroundSupport
 class MyViewController : UIViewController {
     override func loadView() {
         let view = UIView()
-        view.backgroundColor = .white
+        view.backgroundColor = .black
         self.view = view
     }
 
@@ -16,11 +16,33 @@ class MyViewController : UIViewController {
 
     func setup () {
 
+        // Image
         let imgView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
         let img = UIImage(named: "owl.jpg")
         imgView.contentMode = .scaleAspectFit
         imgView.image = img
-        view.addSubview(imgView)
+
+        // Text label
+        let textLabel = UILabel()
+        textLabel.backgroundColor = .yellow
+        textLabel.text = "ThinKong owl"
+//        textLabel.translatesAutoresizingMaskIntoConstraints = true
+        textLabel.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        textLabel.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        textLabel.textAlignment = .center
+
+        // Stack
+        let stackView = UIStackView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 400))
+        stackView.backgroundColor = .green
+        stackView.axis = .vertical
+        stackView.distribution = .equalCentering
+        stackView.alignment = .center
+        stackView.spacing = 5
+
+        stackView.addArrangedSubview(imgView)
+        stackView.addArrangedSubview(textLabel)
+
+        view.addSubview(stackView)
 
     }
 }
